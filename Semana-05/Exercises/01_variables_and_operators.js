faefaecd5f67c9095ce3f44b86297091302d5003
Florencia Eusebi <eusebiflorencia@gmail.com>
2022-04-18 10:27:57 -0300

    BaSP-2022:ex01-variables and operators

---
+++ Semana-05/Exercises/01_variables_and_operators.js
@@ -0,0 +1,30 @@
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS --');
+//a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+console.log('-Exercise 1.a:');
+
+var distanceRosBsAs, distanceBsAsMdq ;
+distanceRosBsAs = 299;
+distanceBsAsMdq = 410;
+var totalDistance = distanceRosBsAs + distanceBsAsMdq;
+
+console.log(totalDistance)
+
+//b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+console.log('-Exercise 1.b:');
+
+var iceCreamFlavour1, iceCreamFlavour2;
+iceCreamFlavour1 = 'strawberry';
+iceCreamFlavour2 = 'chocolate';
+var flavours = iceCreamFlavour1 + ' ' + iceCreamFlavour2;
+
+console.log(flavours)
+
+//c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+console.log('-Exercise 1.c:');
+
+var iceCreamFlavour1, iceCreamFlavour2;
+iceCreamFlavour1 = 'strawberry';
+iceCreamFlavour2 = 'chocolate';
+var totalLength = iceCreamFlavour1.length+iceCreamFlavour2.length;
+
+console.log(totalLength)
