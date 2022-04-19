d348633851a484c8dcdc7a0ec648aeae07185116
Florencia Eusebi <eusebiflorencia@gmail.com>
2022-04-18 22:42:36 -0300

    BaSP-2022:ex03-arrays

--- Semana-05/Exercises/02_strings.js
+++ Semana-05/Exercises/02_strings.js
@@ -1,4 +1,5 @@
 console.log('--EXERCISE 2: STRINGS --');
+
 /* a. Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto
  en mayúscula (utilizar toUpperCase).*/
 console.log('-Exercise 2.a:');
