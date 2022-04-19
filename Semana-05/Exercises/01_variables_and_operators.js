37668eac2afda5a0374b7ce13302a48d6e548644
Florencia Eusebi <eusebiflorencia@gmail.com>
2022-04-18 22:25:08 -0300

    BaSP-2022:ex02-strings

--- Semana-05/Exercises/01_variables_and_operators.js
+++ Semana-05/Exercises/01_variables_and_operators.js
@@ -1,5 +1,6 @@
 console.log('--EXERCISE 1: VARIABLES AND OPERATORS --');
-//a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+/* a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma
+de ambos números en una 3er variable.*/
 console.log('-Exercise 1.a:');
 
 var distanceRosBsAs, distanceBsAsMdq ;
@@ -9,7 +10,7 @@ var totalDistance = distanceRosBsAs + distanceBsAsMdq;
 
 console.log(totalDistance)
 
-//b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 console.log('-Exercise 1.b:');
 
 var iceCreamFlavour1, iceCreamFlavour2;
@@ -19,7 +20,8 @@ var flavours = iceCreamFlavour1 + ' ' + iceCreamFlavour2;
 
 console.log(flavours)
 
-//c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+/* c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado de la suma en una 3er variable (utilizar length).*/
 console.log('-Exercise 1.c:');
 
 var iceCreamFlavour1, iceCreamFlavour2;
