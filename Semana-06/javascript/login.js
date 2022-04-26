f2c5ec767e80d95f5576c10a6b00efa96fc93a80
Florencia Eusebi <eusebiflorencia@gmail.com>
2022-04-25 22:23:52 -0300

    BaSP-W06-2022: JavaScript for login created

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,72 +1,87 @@
-window.onload = function() {
-    var emailInput = document.getElementById('Email');
-    var passwordInput = document.getElementById('Password');
-    var emailOk = false;
-    var passwordOk = false;
+  var emailInput = document.getElementById('email');
+  var passwordInput = document.getElementById('password');
+  var logIn = document.getElementById('login-button');
+  var emailOk = false;
+  var passwordOk = false;
 
-  emailInput.onblur = function() {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value)) {
-        emailOk = true;
-        } else {
-            var loginInput = document.getElementsByClassName("login-email-password");
-            loginInput[0].style = "border-bottom: solid 2px red";
-            errorMsg = 'Invalid Email'
-            var errorSpace = document.getElementById("email-error");
-            var errorMsgBox = document.createElement("p");
-            errorMsgBox.innerHTML = errorMsgEmail;
-            errorMsgBox.style = "color: red; font-size: 16px";
-            errorSpace.appendChild(errorMsgBox);
-            validateEmail = false;
-        };
-      }
+  emailInput.addEventListener('blur', eBlur);
+  emailInput.addEventListener('focus', eFocus);
+  passwordInput.addEventListener('blur', pswBlur);
+  passwordInput.addEventListener('focus', pswFocus);
+  logIn.addEventListener('click', submitUser);
 
-  emailInput.onfocus = function() {
-    emailInput.style = "border-color:none";
+  function eBlur() {
     var errorSpace = document.getElementById("email-error");
-    errorSpace.removeChild(errorSpace.firstElementChild);
-    var loginInput = document.getElementsByClassName("login-email-password");
+    var validate = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value);
+      if (validate) {
+        email.style.color = 'green';
+        emailOk = true;
+        errorSpace.innerText = "";
+          } else {
+                var loginInput = document.getElementsByClassName("login-email-password");
+              loginInput[0].style = "border-bottom: solid 2px red";
+                var errorMsg = 'Please enter a valid Email';
+              errorSpace.innerText = errorMsg;
+              errorSpace.style = "color: red; font-size: 16px";
+              // validateEmail = false;
+          };
+  };
+
+  function eFocus() {
+    emailInput.style = "border-color:none";
+      var errorSpace = document.getElementById("email-error");
+    // errorSpace.removeChild(errorSpace.firstElementChild);
+      var loginInput = document.getElementsByClassName("login-email-password");
     loginInput[0].style =  "border-bottom: solid #49A37B 0.5px";
   };
 
-  passwordInput.onblur = function() {
-    var numbers = /[0-9]/;
-    var alphabet= /[a-z A-Z]/;
-    var resultNumbers = numbers.test(passwordInput.value);
-    var resultAlphabet = alphabet.test(passwordInput.value);
-    if (resultNumbers == true && resultAlphabet == true) {
-        passwordOk = true;
-        } else {
-            var loginInput = document.getElementsByClassName("login-email-password");
-            loginInput[1].style = "border-bottom: solid 2px red";
-            errorMsg = 'Invalid Password'
-            var errorSpace = document.getElementById("password-error");
-            var errorMsgBox = document.createElement("p");
-            errorMsgBox.innerHTML = errorMsgEmail;
-            errorMsgBox.style = "color: red; font-size: 16px";
-            errorMsgBox.appendChild(errorMsgBox);
-            passwordOk = false;
-        };
-  }
-
-  passwordInput.onfocus = function() {
-    passwordInput.style = "border-color: none";
-    var errorMsgBox = document.getElementById("password-error");
-    errorMsgBox.removeChild(errorMsgBox.firstElementChild);
-    var loginInput = document.getElementsByClassName("login-email-password");
-    loginInput[1].style = "border-bottom: solid #49A37B 0.5px";
-  }
-
-  var form = document.getElementById('login-form-id');
-  form.addEventListener('Submit', function(e) {
-    if (emailOk==true && passwordOk==true) {
-        functionModal(emailInput,passwordInput);
-        } else {
-            alert("Please enter a valid Email and Password");
-        }
+  var pswOk;
+  function pswBlur() {
+   pswOk = document.getElementById('password').value;
+    if (pswOk.length > 7) {
+      var countLetters = 0;
+      var countNumbers = 0;
+      for (let i = 0; i < pswOk.length; i++) {
+        var letter = pswOk.substring( i, i + 1);
+        var letterCode = letter.charCodeAt();
+        if (letter == Number(letter)){
+          countNumbers += 1;
+      }
+        if (!((letterCode < 65) || (letterCode > 90 && letterCode < 97) || (letterCode > 122) && (letterCode != 209 && letterCode != 241))) {
+          countLetters += 1;
+      };
     }
-  )
+    if (countLetters + countNumbers == pswOk.length) {
+      passwordOk = true;
+      pswError = document.getElementById('password-error');
+      password.style.color = 'green';
+      pswError.innerText = "";
+    } else {
+      var pswError = document.getElementById('password-error');
+      pswError.innerText = 'The password should not contain special characters';
+      pswError.style = "color: red; font-size: 16px";
+    }
+   } else {
+      pswError = document.getElementById('password-error');
+      pswError.innerText = 'The password should contain more than 7 characters';
+      pswError.style = "color: red; font-size: 16px";
+   }
+  };
 
-  function functionModal(emailInput,passwordInput) {
-    alert("User Email: " + emailInput.value + "User Password: " + passwordInput.value);
-  }
-}
+  function pswFocus() {
+    passwordInput.style = "border-color:none";
+    var pswError = document.getElementById('password-error');
+    // errorSpace.removeChild(errorSpace.firstElementChild);
+    var loginInput = document.getElementsByClassName('login-email-password');
+    loginInput[0].style =  "border-bottom: solid #49A37B 0.5px";
+  };
+
+  function submitUser() {
+    if (emailOk && passwordOk) {
+      alert('Login completed successfully - User Email: ' + emailInput.value + 'User Password: ' + passwordInput.value);
+    } else if (!emailOk) {
+      alert(email.value + 'Email is invalid');
+    } else if (!passwordOk) {
+      alert(password.value + 'Password is invalid');
+    }
+  };
